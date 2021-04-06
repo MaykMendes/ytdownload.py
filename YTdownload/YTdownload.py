613bc0462a666038cd888d3e46598369c96518f0
Mayk Mendes <79224719+MaykMendes@users.noreply.github.com>
2021-04-06 00:54:32 -0300

    Update YTdownload.py
    Rodar o código dentro de uma pasta no editor pra poder baixar o determinado item em sua determinada pasta que será criada quando você  roda o code.

--- YTdownload/YTdownload.py
+++ YTdownload/YTdownload.py
@@ -1,74 +1,170 @@
-from pytube import YouTube
+from pytube import YouTube, Playlist
 from time import sleep
 import os
 import moviepy.editor as mp
 
 
+
+
+# Is Playlist?
+
+
+def is_playlist(url):
+    if len(url) >= 83:
+        if url.find("&list=") == 43: 
+            return True
+    elif len(url) >= 72:
+        if url.find("?list=") == 32: 
+            return True
+    return False
+
+
+# Creat Folder
+
+
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print('Error: Creating directory. ' + directory)
+
+
+# Example
+createFolder('./AudioMP3/')
+
+
+# Loop
 while True:
 
-    # select option
+    # Select Option
     select = input('''Select a option(number) and write below
-    1 - Download Video and audio
-    2 - Download Audio
-    3 - Download Playlist Video
-    write here: ''')
+    1 = Download MP4
+    2 = Download MP3
+    3 = Download MP4 Playlist 
+    4 = Download MP3 Playlist
+    5 = Exit
+    Write here >> ''')
 
-    # don't past link playlist here
-    # download Video
+    # Don't Past Link Playlist Here
+    # Download MP4
     if select == '1':
-        url = input('Past your link here: ')
-        youtube = YouTube(url)
+        url = input("Enter the url of the video: \n>>")
+        video = YouTube(url)
         res = input('''Resolution
-        1 = 360p
-        2 = 720p
-        3 = 1080p
-        write here: ''')
-        if res == 1:
-            youtube.streams.get_by_itag(itag=18).download(
-                output_path='VideoDownloadPython')
-            print(f'downloading {youtube.title}...')
-        elif res == 2:
-            youtube.streams.get_by_itag(itag=22).download(
-                output_path='VideoDownloadPython')
-            print(f'downloading {youtube.title}...')
+    1 = High resolution
+    2 = Medium resolution
+    3 = Low resolution
+    write here: >> ''')
+        if res == '1':
+            print(f"Downloading {video.title}...")
+            video.streams.get_highest_resolution().download(
+            output_path="VideoMP4")
+        elif res == '2':
+            print(f"Downloading {video.title}...")
+            video.streams.get_by_itag(itag= 18).download(
+            output_path="VideoMP4")
+        elif res == 3:
+            print(f"Downloading {video.title}...")
+            video.streams.get_lowest_resolution().download(
+            output_path="VideoMP4")
         else:
-            youtube.streams.get_highest_resolution().download(
-                output_path='VideoDownloadPython')
-            print(f'downloading{youtube.title}...')
-            sleep(2.0)
-            print('download completed as sucess')
+            print("Unavailable option, try again")
+            continue
+        print("Download completed")
 
-    # download Audio
+    # Download MP3
     elif select == '2':
-        url2 = input('Past your link here: ')
-        yt = YouTube(url2)
-        work_folder = os.path.split(__file__)[0]
-        audios = yt.streams.get_audio_only("webm")
-        audios.download(work_folder)
-        v = mp.AudioFileClip(os.path.join(
-            work_folder, audios.default_filename))
-        v.write_audiofile(os.path.join(work_folder, f"{yt.title}.mp3"))
-        print('Downloading...')
-        sleep(2.0)
-        print('download completed as sucess')
+        yt = YouTube(
+            str(input("Enter the URL of the audio you want to download: \n>> ")))
+        
 
-    # download Playlist Videos
+        # extract only audio
+        video = yt.streams.filter(only_audio=True).first()
+
+        # check for destination to save file
+        print(f"Downloading {yt.title}")
+
+        # download the file
+        out_file = video.download(output_path='AudioMP3')
+
+        # save the file
+        base, ext = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
+
+        # result of success
+        print(yt.title + " has been successfully downloaded.")
+
+    # Download Playlist MP4
     elif select == '3':
-        from pytube import YouTube, Playlist
-        url3 = input('Past your link here: ')
-        PLAYLIST_URL = url3
+        url = (
+            str(input("Enter the URL of the playlist video you want to download: \n>> ")))
+        PLAYLIST_URL = url
         playlist = Playlist(PLAYLIST_URL)
-        for url in playlist:
-            video = YouTube(url)
-            print('downloading...')
-            stream = video.streams.get_highest_resolution()
-            stream.download(output_path='Playlist')
-            print('download completed as sucess')
+        res = input('''Resolution
+    1 - High resolution
+    2 - Medium resolution
+    3 - Low resolution
+    write here: >> ''')
+        print(f'downloading playlist: {playlist.title}...')
+        for url in playlist.video_urls:
+            if res == '1':
+                video = YouTube(url)
+                stream = video.streams.get_highest_resolution()
+                stream.download(output_path='VideoPlaylistMP4')
+                print(f'downloading {video.title}...')
+
+            elif res == '2':
+                video = YouTube(url)
+                stream = video.streams.get_by_itag(itag=18)
+                stream.download(output_path='VideoPlaylistMP4')
+                print(f'downloading {video.title}...')
+
+            elif res == '3':
+                video = YouTube(url)
+                stream = video.streams.get_lowest_resolution()
+                stream.download(output_path='VideoPlaylistMP4')
+                print(f'downloading {video.title}...')
+
+            else:
+                print("Unavailable option, try again")
+            continue
+
+        print('\033[1;32mdownload completed as sucess\033[m')
+
+    # Download Playlist MP3
+    elif select == '4':
+        url = (
+            str(input("Enter the URL of the playlist audio you want to download: \n>> ")))
+        PLAYLIST_URL = url
+        pl = Playlist(PLAYLIST_URL)
+        print(f"Downloading Audios of {pl.title}")
+        for url in pl.video_urls:
+            yt = YouTube(url)
+            stream = yt.streams.filter(only_audio= True).first()
+            print(f"Downloading {yt.title}...")
+            out_file = stream.download(output_path='AudioPlaylistMP3')
+            base, ext = os.path.splitext(out_file)
+            new_file = base + '.mp3'
+            os.rename(out_file, new_file)
+        
+        print("Your playlist audios has been succesful downloaded")
+            
+       
+        
+
+    # Exit Programm
+    elif select == '5':
+        print("See you late:)")
+        break
 
     else:
         print('Option invalid please try again.')
+        continue
 
-    cont = str(input("Do you want to continue (y/n) ?")).upper()
+    cont = str(input("Press any key to continue(press 'n' to exit)")).upper()
     if cont == "N":
-        print("Thanks :)")
+        print("See you late :)")
         break
+        
