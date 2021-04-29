e8841e967e52d64b34a5f62d11ebd54a95e37e43
Mayk Mendes <79224719+MaykMendes@users.noreply.github.com>
2021-04-29 19:11:10 -0300

    youtubedownload.py

--- YTdownload/youtubedownload.py
+++ YTdownload/youtubedownload.py
@@ -2,22 +2,23 @@ from pytube import YouTube, Playlist
 from time import sleep
 import os
 import moviepy.editor as mp
+import pyfiglet
 
-
-
+# Banner
+banner = pyfiglet.figlet_format("by Mayk Mendes by D1NNO")
+print(banner)
 
 # Is Playlist?
 
 
 def is_playlist(url):
     if len(url) >= 83:
-        if url.find("&list=") == 43: 
+        if url.find("&list=") == 43:
             return True
     elif len(url) >= 72:
-        if url.find("?list=") == 32: 
+        if url.find("?list=") == 32:
             return True
     return False
-
 
 # Creat Folder
 
@@ -30,7 +31,6 @@ def createFolder(directory):
         print('Error: Creating directory. ' + directory)
 
 
-# Example
 createFolder('./AudioMP3/')
 
 
@@ -59,27 +59,27 @@ while True:
         if res == '1':
             print(f"Downloading {video.title}...")
             try:
-                video.streams.get_by_itag(itag= 37).download(
-                output_path="VideoMP4")
+                video.streams.get_by_itag(itag=37).download(
+                    output_path="VideoMP4")
             except AttributeError:
-                video.streams.get_by_itag(itag= 22).download(
-                output_path="VideoMP4")
+                video.streams.get_by_itag(itag=22).download(
+                    output_path="VideoMP4")
         elif res == '2':
             print(f"Downloading {video.title}...")
             try:
-                video.streams.get_by_itag(itag= 35).download(
-                output_path="VideoMP4")
+                video.streams.get_by_itag(itag=35).download(
+                    output_path="VideoMP4")
             except AttributeError:
-                video.streams.get_by_itag(itag= 18).download(
-                output_path="VideoMP4")
+                video.streams.get_by_itag(itag=18).download(
+                    output_path="VideoMP4")
         elif res == '3':
             print(f"Downloading {video.title}...")
             try:
                 video.streams.get_by_itag(5).download(
-                output_path="VideoMP4")
+                    output_path="VideoMP4")
             except AttributeError:
                 video.streams.get_lowest_resolution().download(
-                output_path="VideoMP4")
+                    output_path="VideoMP4")
         else:
             print("Unavailable option, try again")
             continue
@@ -89,27 +89,29 @@ while True:
     elif select == '2':
         yt = YouTube(
             str(input("Enter the URL of the audio you want to download: \n>> ")))
-        
 
-        # extract only audio
-        video = yt.streams.filter(only_audio=True).first()
+        try:
+            # extract only audio
+            video = yt.streams.filter(only_audio=True).first()
 
-        # check for destination to save file
-        print(f"Downloading {yt.title}")
+            # check for destination to save file
+            print(f"Downloading {yt.title}")
 
-        # download the file
-        out_file = video.download(output_path='AudioMP3')
+            # download the file
+            out_file = video.download(output_path='AudioMP3')
 
-        # save the file
-        base, ext = os.path.splitext(out_file)
-        new_file = base + '.mp3'
-        os.rename(out_file, new_file)
+            # save the file
+            base, ext = os.path.splitext(out_file)
+            new_file = base + '.mp3'
+            os.rename(out_file, new_file)
 
-        # result of success
-        print(yt.title + " has been successfully downloaded.")
+            # result of success
+            print(yt.title + " has been successfully downloaded.")
+        except FileExistsError:
+            print("\033]mThat Audio file already exists\033[m")
 
     # Download Playlist MP4
-   elif select == '3':
+    elif select == '3':
         url = (
             str(input("Enter the URL of the playlist video you want to download: \n>> ")))
         is_playlist(url)
@@ -126,11 +128,11 @@ while True:
                 try:
                     video = YouTube(url)
                     print(f'downloading {video.title}...')
-                    stream = video.streams.get_by_itag(itag= 37)
+                    stream = video.streams.get_by_itag(itag=37)
                     stream.download(output_path='VideoPlaylistMP4')
                 except AttributeError:
                     video = YouTube(url)
-                    stream = video.streams.get_by_itag(itag= 22)
+                    stream = video.streams.get_by_itag(itag=22)
                     stream.download(output_path='VideoPlaylistMP4')
 
             elif res == '2':
@@ -141,14 +143,14 @@ while True:
                     stream.download(output_path='VideoPlaylistMP4')
                 except AttributeError:
                     video = YouTube(url)
-                    stream = video.streams.get_by_itag(itag= 18)
+                    stream = video.streams.get_by_itag(itag=18)
                     stream.download(output_path='VideoPlaylistMP4')
 
             elif res == '3':
                 try:
                     video = YouTube(url)
                     print(f'downloading {video.title}...')
-                    stream = video.streams.get_by_itag(itag= 5)
+                    stream = video.streams.get_by_itag(itag=5)
                     stream.download(output_path='VideoPlaylistMP4')
                 except AttributeError:
                     video = YouTube(url)
@@ -165,27 +167,25 @@ while True:
     elif select == '4':
         url = (
             str(input("Enter the URL of the playlist audio you want to download: \n>> ")))
+        is_playlist(url)
         PLAYLIST_URL = url
         pl = Playlist(PLAYLIST_URL)
         print(f"Downloading Audios of {pl.title}")
         for url in pl.video_urls:
             yt = YouTube(url)
-            stream = yt.streams.filter(only_audio= True).first()
+            stream = yt.streams.filter(only_audio=True).first()
             print(f"Downloading {yt.title}...")
             out_file = stream.download(output_path='AudioPlaylistMP3')
             base, ext = os.path.splitext(out_file)
             new_file = base + '.mp3'
             os.rename(out_file, new_file)
-        
+
         print("Your playlist audios has been succesful downloaded")
-            
-       
-        
 
     # Exit Programm
     elif select == '5':
         print("See you late:)")
-        exit()
+        break
 
     else:
         print('Option invalid please try again.')
@@ -195,4 +195,3 @@ while True:
     if cont == "N":
         print("See you late :)")
         break
-        
